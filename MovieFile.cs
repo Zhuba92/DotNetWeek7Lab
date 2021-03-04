86629e2e03c7ada18159834b01b5cab4509125e2
zachary <zhuba@my.wctc.edu>
2021-03-03 20:29:15 -0600

    Some progress

--- MovieFile.cs
+++ MovieFile.cs
@@ -1,11 +1,88 @@
 using System;
 using NLog.Web;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DotNetWeek7Lab
 {
-    class MovieFile
+    public class MovieFile : Movie
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+        public string filePath{get; set;}
+        public List<Movie> Movies {get; set;}
+
+        public MovieFile(string movieFilePath)
+        {
+            filePath = movieFilePath;
+            Movies = new List<Movie>();
+
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                 while (!sr.EndOfStream)
+                {
+                    Movie movie = new Movie();
+                    string line = sr.ReadLine();
+                    int idx = line.IndexOf('"');
+                    if (idx == -1)
+                    {
+                        string[] movieDetails = line.Split(',');
+                        movie.mediaId = UInt64.Parse(movieDetails[0]);
+                        movie.title = movieDetails[1];
+                        movie.genres = movieDetails[2].Split('|').ToList();
+                        movie.director = movieDetails[3];
+                        movie.runningTime = TimeSpan.Parse(movieDetails[4]);
+                    }
+                    else
+                    {
+                        movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
+                        line = line.Substring(idx + 1);
+                        idx = line.IndexOf('"');
+                        movie.title = line.Substring(0, idx);
+                        line = line.Substring(idx + 2);
+                        movie.genres = line.Split('|').ToList();
+                    }
+                    Movies.Add(movie);
+                }
+                sr.Close();
+                logger.Info("Movies in file {Count}", Movies.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+
+        public bool isUniqueTitle(string title)
+        {
+            if (Movies.ConvertAll(m => m.title.ToLower()).Contains(title.ToLower()))
+            {
+                logger.Info("Duplicate movie title {Title}", title);
+                return false;
+            }
+            return true;
+        }
+
+         public void AddMovie(Movie movie)
+        {
+            try
+            {
+                // first generate movie id
+                movie.mediaId = Movies.Max(m => m.mediaId) + 1;
+                StreamWriter sw = new StreamWriter(filePath, true);
+                sw.WriteLine($"{movie.mediaId},{movie.title},{string.Join("|", movie.genres)}");
+                sw.Close();
+                // add movie details to Lists
+                Movies.Add(movie);
+                // log transaction
+                logger.Info("Movie id {Id} added", movie.mediaId);
+            } 
+            catch(Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,46 +10,56 @@ namespace DotNetWeek7Lab
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            string movieFilePath = "movies.scrubbed.csv";
 
             logger.Info("Program started");
+            MovieFile movieFile = new MovieFile(movieFilePath);
+            string choice = "";
+            do
+            {
+                Console.WriteLine("1) Add Movie\n2) Display All Movies\nEnter to quit");
+                choice = Console.ReadLine();
+                logger.Info("User choice: {Choice}", choice);
 
-            // Movie movie = new Movie
-            // {
-            //     mediaId = 123,
-            //     title = "Greatest Movie Ever, The (2020)",
-            //     director = "Jeff Grissom",
-            //     // timespan (hours, minutes, seconds)
-            //     runningTime = new TimeSpan(2, 21, 23),
-            //     genres = { "Comedy", "Romance" }
-            // };
+                if(choice == "1")
+                {
+                    Movie movie = new Movie();
+                    Console.WriteLine("Enter movie title");
+                    movie.title = Console.ReadLine();
+                    Console.WriteLine("Enter the director of the movie: ");
+                    movie.director = Console.ReadLine();
+                    Console.WriteLine("Enter movie runtime (00:00:00): ");
+                    movie.runningTime = TimeSpan.Parse(Console.ReadLine());
+                    if (movieFile.isUniqueTitle(movie.title)){
+                        string input;
+                        do
+                        {
+                            Console.WriteLine("Enter genre (or done to quit)");
+                            input = Console.ReadLine();
+                            if (input != "done" && input.Length > 0)
+                            {
+                                movie.genres.Add(input);
+                            }
+                        } while (input != "done");
+                        if (movie.genres.Count == 0)
+                        {
+                            movie.genres.Add("(no genres listed)");
+                        }
+                        movieFile.AddMovie(movie);
+                    }
+                }
+                else if(choice == "2")
+                {
+                    foreach(Movie m in movieFile.Movies)
+                    {
+                        Console.WriteLine(m.Display());
+                    }
+                }
 
-            // Console.WriteLine(movie.Display());
 
-            // Album album = new Album
-            // {
-            //     mediaId = 321,
-            //     title = "Greatest Album Ever, The (2020)",
-            //     artist = "Jeff's Awesome Band",
-            //     recordLabel = "Universal Music Group",
-            //     genres = { "Rock" }
-            // };
-            // Console.WriteLine(album.Display());
-
-            // Book book = new Book
-            // {
-            //     mediaId = 111,
-            //     title = "Super Cool Book",
-            //     author = "Jeff Grissom",
-            //     pageCount = 101,
-            //     publisher = "",
-            //     genres = { "Suspense", "Mystery" }
-            // };
-            // Console.WriteLine(book.Display());
+            }while (choice == "1" || choice == "2");
 
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-            logger.Info(scrubbedFile);
-
-            logger.Info("Program ended");
         }
     }
 }
